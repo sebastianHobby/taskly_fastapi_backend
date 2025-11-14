d7a23a9c98b18c30271553fff7567430f015bc00
sebastian <seb@test.com>
2025-11-14 22:31:16 +1100

    Updated project structure. Previous version was unnecessarily complex.

--- src/main.py
+++ src/main.py
@@ -1,14 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
 from src.core.database import create_database_and_tables
+
 from src.routes.task_routes import task_router
 from src.routes.project_routes import project_router
 from src.routes.area_routes import area_router
 
 # Create required tables
 create_database_and_tables()
-# TODO fix link project to GIT for soruce control before you lose it somehow
 
 app = FastAPI()
 # CORS setup
@@ -27,6 +26,3 @@ app.add_middleware(
 app.include_router(task_router, tags=["tasks"])
 app.include_router(area_router, tags=["areas"])
 app.include_router(project_router, tags=["projects"])
-
-# @app.on_event("startup")
-# def on_startup():
